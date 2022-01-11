f0455165845cc7c7342a9d7bf3c73d5b67600e9e
GeorgiyEloev <geloev.exceedteam@gmail.com>
2022-01-11 10:34:14 +0300

    fix layout

--- app.js
+++ app.js
@@ -1,18 +1,12 @@
 const express = require("express");
 const cors = require("cors");
-const mongoose = require("mongoose");
 const app = express();
 
-const apiRoutes = require("./src/modules/routes/routes");
 
 app.use(cors());
 
-const url =
-  "mongodb+srv://user:user@cluster0.fiygj.mongodb.net/TestEducationDB?retryWrites=true&w=majority";
-mongoose.connect(url);
-
 app.use(express.json());
-app.use("/", apiRoutes);
+app.use("/");
 
 app.listen(8000, () => {
   console.log("listening on port 8000!");
