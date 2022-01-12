5b4eb7dbe4d75d22e92a0a7690e77374aaed4071
GeorgiyEloev <geloev.exceedteam@gmail.com>
2022-01-12 14:31:35 +0300

    fix style code

--- src/modules/routes/record.router.js
+++ src/modules/routes/record.router.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const { getAllRecord
-} = require("../conrtollers/record.controller");
+const { getAllRecord } = require("../conrtollers/record.controller");
 
 router.get("/allRecord", getAllRecord);
 
