ad77b6fc24011ac7eab9ca62c58853fdfb0c74c5
GeorgiyEloev <geloev.exceedteam@gmail.com>
2022-01-12 15:12:28 +0300

    fix

--- src/modules/routes/record.router.js
+++ src/modules/routes/record.router.js
@@ -1,12 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-const {
-  getAllRecord,
-  addNewRecord,
-} = require("../conrtollers/record.controller");
+const { getAllRecord } = require("../conrtollers/record.controller");
 
 router.get("/allRecord", getAllRecord);
-router.post("/addNewRecord", addNewRecord);
 
 module.exports = router;
