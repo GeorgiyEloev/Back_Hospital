f3eb4737854f0948bef38f192beeb12f6c2f884c
GeorgiyEloev <geloev.exceedteam@gmail.com>
2022-01-12 17:06:55 +0300

    add token decode

--- src/modules/conrtollers/record.controller.js
+++ src/modules/conrtollers/record.controller.js
@@ -1,12 +1,16 @@
+const jwt = require("jsonwebtoken");
 const Record = require("../../db/models/reception/recordSchema");
 
 module.exports.getAllRecord = (req, res) => {
-  const { userId } = req.body;
-  Record.find({ userId })
-    .then((result) => {
-      res.send({ data: result });
-    })
-    .catch((err) => {
-      res.status(419).send("Error. An error occurred during the search!!!");
-    });
+  const { authorization } = req.headers;
+  jwt.verify(authorization, process.env.JWT_KEY, (err, data) => {
+    if (err) return res.status(401).send("Error, corrupted token!!!");
+    Record.find({ userId: data._id })
+      .then((result) => {
+        res.send({ data: result });
+      })
+      .catch((err) => {
+        res.status(419).send("Error. An error occurred during the search!!!");
+      });
+  });
 };
