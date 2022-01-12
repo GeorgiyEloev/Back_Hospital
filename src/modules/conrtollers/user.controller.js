2b27ab87b544bbebd0ed53a8ab27595ad1c09f4e
GeorgiyEloev <geloev.exceedteam@gmail.com>
2022-01-12 10:19:55 +0300

    fix .env and catch

--- src/modules/conrtollers/user.controller.js
+++ src/modules/conrtollers/user.controller.js
@@ -1,36 +1,45 @@
 const crypto = require("crypto");
-const _ = require("lodash");
 const User = require("../../db/models/user/userSchema");
 const tokenService = require("../service/token-service");
 
 module.exports.addNewUser = (req, res, next) => {
-  User.findOne({ login: req.body.login }).then((result) => {
-    if (!result) {
-      if (
-        req.body.hasOwnProperty("login") &&
-        req.body.hasOwnProperty("password") &&
-        req.body.password.trim() &&
-        req.body.login.trim()
-      ) {
-        const user = new User(req.body);
-        user.password = crypto
-          .createHash("sha256")
-          .update(user.password)
-          .digest("base64");
-        user.save().then(() => {
-          const { _id, login } = user;
-          const userNotPassword = {
-            _id,
-            login,
-          };
-          const token = tokenService.generateToken({ ...userNotPassword });
-          res.send({ data: { login }, token });
-        });
+  User.findOne({ login: req.body.login })
+    .then((result) => {
+      if (!result) {
+        if (
+          req.body.hasOwnProperty("login") &&
+          req.body.hasOwnProperty("password") &&
+          req.body.password.trim() &&
+          req.body.login.trim()
+        ) {
+          const user = new User(req.body);
+          const { HASH_ALGOR, HASH_BASE } = process.env;
+          user.password = crypto
+            .createHash(HASH_ALGOR)
+            .update(user.password)
+            .digest(HASH_BASE);
+          user
+            .save()
+            .then((result) => {
+              const { _id, login } = result;
+              const userNotPassword = {
+                _id,
+                login,
+              };
+              const token = tokenService.generateToken({ ...userNotPassword });
+              res.send({ data: { login }, token });
+            })
+            .catch((err) => {
+              res.status(421).send("Error, user doesn't save!!!");
+            });
+        } else {
+          res.status(422).send("Error, incorrect data!!!");
+        }
       } else {
-        res.status(422).send("Error, incorrect data!!!");
+        res.status(420).send("Error, login is busy!!!");
       }
-    } else {
-      res.status(420).send("Error, login is busy!!!");
-    }
-  });
+    })
+    .catch((err) => {
+      res.status(419).send("Error. An error occurred during the search!!!");
+    });
 };
