e53a3e53e8152ee0b02391b43052b8959b215bcf
GeorgiyEloev <geloev.exceedteam@gmail.com>
2022-02-03 17:33:56 +0300

    fix

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-const { graphqlHTTP } = require("express-graphql");
-const schema = require("./src/graphQLServer/schema");
-const resolvers = require("./src/graphQLServer/root/resolversRecord");
-const resolversUser = require("./src/graphQLServer/root/resolversUser");
 
 const apiReceptionRoutes = require("./src/modules/routes/record.router");
 const apiUserRoutes = require("./src/modules/routes/user.router");
@@ -15,19 +11,6 @@ const app = express();
 app.use(cors());
 
 mongoose.connect(UTL_BD);
-
-const dbConnection = mongoose.connection;
-dbConnection.on("error", (err) => console.log(`Connection error: ${err}`));
-dbConnection.once("open", () => console.log("Connected to DB!"));
-
-app.use(
-  "/graphql",
-  graphqlHTTP({
-    graphiql: true,
-    schema,
-    rootValue: { ...resolvers, ...resolversUser },
-  })
-);
 
 app.use(express.json());
 app.use("/record/", apiReceptionRoutes);
